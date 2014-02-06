ca480b124800cebe3a9e8b7ff84f5e679aa38351
Reena <lthaveet@macalester.edu>
2014-02-06 17:11:42 -0600

    hw01

--- test/edu/macalester/comp124/hw1/UnitConversionsTest.java
+++ test/edu/macalester/comp124/hw1/UnitConversionsTest.java
@@ -18,4 +18,17 @@ public class UnitConversionsTest {
         assertEquals( 98.6, UnitConversions.celciusToFahrenheit( 37), 0.00001);
         assertEquals(212,   UnitConversions.celciusToFahrenheit(100), 0.00001);
     }
+    @Test
+    public void testMeterToInches() {
+        assertEquals(  0, UnitConversions.meterToInches( 0 ), 0.00001);
+        assertEquals( 196.85, UnitConversions.meterToInches( 5), 0.00001);
+        assertEquals(1377.95, UnitConversions.meterToInches(35), 0.00001);
+    }
+
+    @Test
+    public void testInchesToMeter() {
+        assertEquals( 0,   UnitConversions.inchesToMeter(  0), 0.00001);
+        assertEquals( 5, UnitConversions.inchesToMeter( 196.85), 0.00001);
+        assertEquals(35,   UnitConversions.inchesToMeter(1377.95), 0.00001);
+    }
 }
